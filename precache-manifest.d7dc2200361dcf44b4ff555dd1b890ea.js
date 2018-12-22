351aa133604b0d63c97915875251dcf11a8e6c51
elinnylund <enylund@abo.fi>
2018-12-22 12:29:24 +0200

    Updates

---
+++ precache-manifest.d7dc2200361dcf44b4ff555dd1b890ea.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "71c944652c771464dc9a",
+    "url": "/2018-interactive-web-apps-project-elinnylund/static/js/runtime~main.71c94465.js"
+  },
+  {
+    "revision": "fb59f3943c1a3f7f2679",
+    "url": "/2018-interactive-web-apps-project-elinnylund/static/js/main.fb59f394.chunk.js"
+  },
+  {
+    "revision": "1438883c0901be04a688",
+    "url": "/2018-interactive-web-apps-project-elinnylund/static/js/1.1438883c.chunk.js"
+  },
+  {
+    "revision": "fb59f3943c1a3f7f2679",
+    "url": "/2018-interactive-web-apps-project-elinnylund/static/css/main.a0973674.chunk.css"
+  },
+  {
+    "revision": "1438883c0901be04a688",
+    "url": "/2018-interactive-web-apps-project-elinnylund/static/css/1.fae81bac.chunk.css"
+  },
+  {
+    "revision": "e5e737c5f752567afb965ca2fe6d629a",
+    "url": "/2018-interactive-web-apps-project-elinnylund/index.html"
+  }
+];
